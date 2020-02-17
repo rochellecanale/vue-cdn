340dc291abb10b2acd561a068e0b2a790f485bd8
Rochelle Yang Canale <chelle@cbo.me>
2020-02-17 12:18:12 +0800

    Unfinished hashcat

---
+++ exercise/hackathon/js/test.js
@@ -0,0 +1,6 @@
+let x = 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb';
+let y = '3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d';
+let z = 'abc';
+let charcode = z.charCodeAt(1);
+
+console.log(btoa("Hello, World"));
